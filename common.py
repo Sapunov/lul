04f7a6e8d0d716ea79f513f8bbf011888beb2f7a
Nikita Sapunov <kiton1994@gmail.com>
2017-10-03 23:05:11 +0300

    Некоторые функции вынесены в отдельные модули

--- common.py
+++ common.py
@@ -3,75 +3,12 @@ import string as string_lib
 
 from nltk.tokenize import word_tokenize
 
-
-NUMBER = r"([0-9]+)(\.[0-9]+)?"
-SPACE = r"[ \n\t]"
-
-HALF_HOUR = r"полчас(а|ам|ами|ах|е|ой|у|ы)?"
-HOUR = r"час(а|ам|ами|ах|е|ов|ом|у|ы)?"
-MINUTE = r"минут(а|ам|ами|ах|е|ой|у|ы)?"
-SECOND = r"секунд(а|ам|ами|ах|е|ой|у|ы)?"
-
-TODAY = r"сегодня"
-YESTERDAY = r"вчера"
-
-AUGUST = r"август(а|ам|ами|ах|е|ов|ом|у|ы)?"
-APRIL = r"апрел(е|ей|ем|и|ь|ю|я|ям|ями|ях)"
-DECEMBER = r"декабр(ь|и|я|ей|ю|ям|ём|ями|е|ях)"
-JULY = r"июл(ь|и|я|ей|ю|ям|ем|ями|е|ях)"
-JUNE = r"июн(ь|и|я|ей|ю|ям|ем|ями|е|ях)"
-MAY = r"ма(й|и|я|ев|ю|ям|ем|ями|е|ях)"
-MARCH = r"март(ы|а|ов|у|ам|ом|ами|е|ах)?"
-NOVEMBER = r"ноябр(ь|и|я|ей|ю|ям|ём|ями|е|ях)"
-OCTOBER = r"октябр(ь|и|я|ей|ю|ям|ём|ями|е|ях)"
-SEPTEMBER = r"сентябр(ь|и|я|ей|ю|ям|ём|ями|е|ях)"
-FEBRUARY = r"феврал(ь|и|я|ей|ю|ям|ём|ями|е|ях)"
-JANUARY = r"январ(ь|и|я|ей|ю|ям|ём|ями|е|ях)"
-
-WEIGHT = r"ве(с|са|сов|су|сам|сом|сами|се|сах|сить|шу)"
+from constants import NUM_POINTER, DATE_POINTER, TIME_POINTER, WEIGHT_POINTER
+from constants import NUMBER, WEIGHT, UI
+from extractors import find_date, find_time, find_weight
 
 
-def find_time(string):
-
-    time_word = r"({0}|{1}|{2}|{3})".format(HOUR, MINUTE, SECOND, HALF_HOUR)
-    time = r"({0}{1}{2}|{2}{1}{0}|{2})".format(NUMBER, SPACE, time_word)
-
-    result = re.search(time, string, re.UNICODE | re.IGNORECASE)
-
-    if result is not None:
-        return result.group()
-
-
-def find_date(string):
-
-    date_word = "(" + (r"|".join("{{{0}}}".format(i) for i in range(12))).format(
-        AUGUST, APRIL, DECEMBER, JULY, JUNE, MAY, MARCH,
-        NOVEMBER, OCTOBER, SEPTEMBER, FEBRUARY, JANUARY) + ")"
-
-    date_relative = r"({0}|{1})".format(TODAY, YESTERDAY)
-
-    date = r"({0}{1}{2})|{2}{1}{0}|{3}".format(NUMBER, SPACE, date_word, date_relative)
-
-    result = re.search(date, string, re.UNICODE | re.IGNORECASE)
-
-    if result is not None:
-        return result.group()
-
-
-def find_weight(string):
-
-    weight_result = re.search(WEIGHT, string, re.UNICODE | re.IGNORECASE)
-
-    if not weight_result:
-        return None
-
-    number_result = re.search(NUMBER, string)
-
-    if number_result:
-        return number_result.group()
-
-
-def replace_datepointer(string, replace_on="DATEPOINTER"):
+def replace_datepointer(string, replace_on=DATE_POINTER):
 
     pointer = find_date(string)
 
@@ -81,7 +18,7 @@ def replace_datepointer(string, replace_on="DATEPOINTER"):
     return string
 
 
-def replace_timepointer(string, replace_on="TIMEPOINTER"):
+def replace_timepointer(string, replace_on=TIME_POINTER):
 
     pointer = find_time(string)
 
@@ -91,13 +28,13 @@ def replace_timepointer(string, replace_on="TIMEPOINTER"):
     return string
 
 
-def replace_weightpointer(string, replace_on="WEIGHTPOINTER"):
+def replace_weightpointer(string, replace_on=WEIGHT_POINTER):
 
     weight = find_weight(string)
 
     # Если нашли именно вес, то ставим указатель
     if weight:
-        string = re.sub(WEIGHT, replace_on, string, re.UNICODE | re.IGNORECASE)
+        string = re.sub(WEIGHT, replace_on, string, UI)
 
         # И удаляем цифру чтобы не смущать классификатор
         string = re.sub(NUMBER, "", string)
@@ -105,7 +42,7 @@ def replace_weightpointer(string, replace_on="WEIGHTPOINTER"):
     return string
 
 
-def replace_numbers(string, replace_on="NUM"):
+def replace_numbers(string, replace_on=NUM_POINTER):
 
     return re.sub(NUMBER, replace_on, string)
 
@@ -114,8 +51,8 @@ def replace_letterdigits(string):
 
     tokens = word_tokenize(string)
 
-    for i in range(len(tokens)):
-        match = re.match(r"^(([0-9]+)(\.[0-9]+)?)(k|к)$", tokens[i], re.UNICODE)
+    for i, token  in enumerate(tokens):
+        match = re.match(r"^({0})(k|к)$".format(NUMBER), token, re.UNICODE)
 
         if match:
             tokens[i] = str(int(float(match.group(1)) * 1000))
